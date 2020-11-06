8a040bd277f2a378ad0d065bc16cd208e6555cf1
Heber <hebergonza@gmail.com>
2020-11-06 18:24:52 -0500

    finished histo.py

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,33 @@
 # Your code here
+import re
 
+with open("robin.txt") as f:
+    words = f.read()
+
+words = words.lower()
+
+pattern = '[\"\:\;\,\.\-\+\=\/\\\|[\]\{\}\(\)\*\^\&]'
+words = re.sub(pattern, '', words)
+
+words_split = words.split()
+
+biggest_word = 0
+for word in words_split:
+	if len(word) > biggest_word:
+		biggest_word = len(word)
+
+lookup = {}
+for word in words_split:
+	if word not in lookup:
+		lookup[word] = "#"
+	else:
+		lookup[word] = lookup[word] + "#"
+
+list_lookup = list(lookup.items())
+
+list_lookup.sort()
+
+list_lookup.sort(key=lambda tuple_pair: tuple_pair[1], reverse=True)
+
+for i in list_lookup:
+	print(i[0] + "  " + (" " * (biggest_word - len(i[0]))) + i[1])
