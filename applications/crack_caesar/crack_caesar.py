2d117b6d231a7c56dbe49f88b5f765bcb3237414
Heber <hebergonza@gmail.com>
2020-11-06 23:29:36 -0500

    finished crack_caesar.py

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,42 @@
 # decode it.
 
 # Your code here
+with open("ciphertext.txt") as f:
+  text = f.read()
 
+char_list = [char for char in text]
+
+char_dict = {}
+
+for char in char_list:
+  if char in char_dict and char.isalpha():
+    char_dict[char] = char_dict[char] + 1
+  elif char not in char_dict and char.isalpha():
+    char_dict[char] = 1
+
+list_char_dict = list(char_dict.items())
+
+list_char_dict.sort(key=lambda tuple_pair: tuple_pair[1], reverse=True)
+
+freq_list = []
+for x in range(len(list_char_dict)):
+  freq_list.append(list_char_dict[x][0])
+
+freq_list.pop()
+
+freq_analysis = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+decode_table = {}
+for x in range(len(freq_list)):
+  decode_table[freq_list[x]] = freq_analysis[x]
+
+# print(char_list)
+new_text = ''
+
+for char in char_list:
+  if char.isalpha() and char != 'â':
+    new_text = new_text + decode_table[char]
+  else:
+    new_text = new_text + char
+
+print(new_text)
